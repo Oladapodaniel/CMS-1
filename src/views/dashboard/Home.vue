34814413b958ccb89f3787091e275f47e2c311f7
Ogbara Gerrald Godstar <sengodstar@gmail.com>
2020-11-19 17:22:41 +0100

    Remove vertical scroll bar

--- src/views/dashboard/Home.vue
+++ src/views/dashboard/Home.vue
@@ -1,106 +1,115 @@
 <template>
-    <div class="whole-page">
-        <div class="links-menu" :class="{ 'show': menuShouldShow }">
-            <MenuLinks />
-        </div>
-        <div class="main-con dim" @click="hideMenu">
-            <router-view></router-view>
-        </div>
+  <div class="whole-page">
+    <div class="links-menu" :class="{ show: menuShouldShow }">
+      <MenuLinks />
     </div>
-    <div class="toggle" @click="toggleMenu">
-        <i class="fa fa-bars"></i>
+    <div class="main-con dim" @click="hideMenu">
+      <router-view></router-view>
     </div>
+  </div>
+  <div class="toggle" @click="toggleMenu">
+    <i class="fa fa-bars"></i>
+  </div>
 </template>
 
 <script>
-    import { ref } from "vue";
-    import MenuLinks from "../../components/nav/MenuLinks.vue";
+import { ref } from "vue";
+import MenuLinks from "../../components/nav/MenuLinks.vue";
 
-    export default {
-        components: { MenuLinks },
+export default {
+  components: { MenuLinks },
 
-        setup() {
-            const menuShouldShow = ref(false);
+  setup() {
+    const menuShouldShow = ref(false);
 
-            const toggleMenu = () => menuShouldShow.value = !menuShouldShow.value;
+    const toggleMenu = () => (menuShouldShow.value = !menuShouldShow.value);
 
-            const hideMenu = () => {
-                if (menuShouldShow.value)
-                    menuShouldShow.value = false;
-            }
+    const hideMenu = () => {
+      if (menuShouldShow.value) menuShouldShow.value = false;
+    };
 
-            return {
-                menuShouldShow,
-                toggleMenu,
-                hideMenu
-            }
-        }
-    }
+    return {
+      menuShouldShow,
+      toggleMenu,
+      hideMenu,
+    };
+  },
+};
 </script>
 
 <style scoped>
-    * {
-        box-sizing: border-box;
-    }
+* {
+  box-sizing: border-box;
+}
 
-    .whole-page {
-        position: relative;
-        display: flex;
-        min-height: 100vh;
-        overflow: auto;
-        background: #fff;
-    }
+.whole-page {
+  position: relative;
+  display: flex;
+  min-height: 100vh;
+  overflow: auto;
+  background: #fff;
+}
 
-    .links-menu {
-        width: 20%;
-        max-width: 287px;
-        height: 100%;
-        /* position: absolute; */
-    }
+.links-menu {
+  width: 20%;
+  max-width: 287px;
+  height: 100%;
+  /* position: absolute; */
+}
 
-    .main-con {
-        width: 80%;
-        max-width: 1200px;
-        margin: 0 auto;
-        height: 100%;
-        max-height: fit-content;
-        background: #fff;
-        overflow-y: auto;
-        padding-top: 20px;
-    }
+.main-con {
+  width: 80%;
+  max-width: 1200px;
+  margin: 0 auto;
+  height: 100%;
+  max-height: fit-content;
+  background: #fff;
+  overflow-y: auto;
+  padding-top: 20px;
+}
 
-    .toggle {
-        display: none;
-        width: 30px;
-        text-align: center;
-        position: absolute;
-        top: 4px;
-        right: 8px;
-        font-size: 30px;
-    }
+.main::-webkit-scrollbar {
+  display: none;
+}
 
-    .toggle:hover {
-        cursor: pointer;
-    }
+/* Hide scrollbar for IE, Edge and Firefox */
+.main {
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+}
 
-    @media screen and (max-width: 1100px) {
-        .toggle {
-            display: block
-        }
+.toggle {
+  display: none;
+  width: 30px;
+  text-align: center;
+  position: absolute;
+  top: 4px;
+  right: 8px;
+  font-size: 30px;
+}
 
-        .links-menu {
-            position: fixed;
-            left: -287px;
-            z-index: 9;
-        }
+.toggle:hover {
+  cursor: pointer;
+}
 
-        .links-menu.show {
-            position: fixed;
-            left: 0;
-        }
+@media screen and (max-width: 1100px) {
+  .toggle {
+    display: block;
+  }
 
-        .main-con {
-            width: 100%;
-        }
-    }
+  .links-menu {
+    position: fixed;
+    left: -287px;
+    z-index: 9;
+  }
+
+  .links-menu.show {
+    position: fixed;
+    left: 0;
+  }
+
+  .main-con {
+    width: 100%;
+  }
+}
 </style>
