213bca7fbe34da81c382fca22d1df57c7414a007
okon ifiok friday <wisdominterconnect@gmail.com>
2021-01-22 16:56:56 +0100

    working on terms of service

--- src/views/account/TermsOfService.vue
+++ src/views/account/TermsOfService.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="contain">
-            <div class="container-terms">
+            <div class="container-terms pl-4">
                 <div class="container">
                     <h1 class="mt-5">Terms of Service
                         <hr class="mt-0" style="width:10%;text-align:left;margin-left:0;background-color:#136ACD;border-width:3px;border-radius:2px">
@@ -10,7 +10,7 @@
               </div>
             
             </div>
-            <div class="terms-body container pb-5">
+            <div class="terms-body container pb-5 pl-5 pr-5">
                 <div class="cirle-container">
                 <span class="dot mt-2"></span> <h1 class="ml-2" style="font-size:22px; font-weight:bold">Terms Of Service</h1>
                 </div>
@@ -39,12 +39,11 @@
                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum impedit corporis consectetur molestias ipsa sint blanditiis, dicta maiores! Laudantium debitis blanditiis voluptatum error quis deserunt quasi quos eum sapiente totam.
                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum impedit corporis consectetur molestias ipsa sint blanditiis, dicta maiores! Laudantium debitis blanditiis voluptatum error quis deserunt quasi quos eum sapiente totam
                 </p>
-                <div class="buton ">
+            </div>
+            <div class="buton">
                  <Button label="UNDERSTOOD" class="p-button-raised p-button-rounded"  style="font-size:14px; opacity:1; color:#ffffff; background:#136ACD; border-color: #136ACD;width:156px; height:45px" />
                  
              </div>
-
-            </div>
         </div>
 
     </div>
@@ -64,7 +63,10 @@ background: #FFFFFF 0% 0% no-repeat padding-box;
 border: 1px solid #EBEFF4;
 border-radius: 20px;
 opacity: 1;
-width: 100%;
+width: 90%;
+margin: auto;
+max-width:1106px;
+
 }
 .container-terms{
 background: #FFFFFF 0% 0% no-repeat padding-box;
@@ -84,22 +86,164 @@ opacity: 1;
 
 }
 .dot {
-  height: 7px;
-  width: 7px;
+  height: 12px;
+  width: 12px;
   background-color:#136ACD;
   border-radius: 50%;
   display: inline-block;
 }
 .cirle-container{
-    display: flex;
+display: flex;
 }
 .buton{
+background: #FFFFFF 0% 0% no-repeat padding-box;
+border: 1px solid #EBEFF4;
+border-radius: 0px 0px 20px 20px;
+opacity: 1;
 display: flex;
-justify-content:flex-end ;
+justify-content:flex-end;
+padding: 20px 0px;
 bottom: 0;
 position: fixed;
-right: 9%;
-margin-bottom: 20px;
+width: 64%;
 }
+@media only screen and (max-width: 760px) {
+.buton{
+width: 90%;
+}
+.p-button{
+    right:15px;
+}
+}
+@media (min-width: 761px) and (max-width:1230px) {
+.buton{
+width: 90%;
+}
+.p-button{
+    right:15px;
+}
+}
+@media (min-width: 1231px) and (max-width:1256px) {
+.buton{
+width: 88%;
+}
+.p-button{
+    right:15px;
+}
+}
+@media (min-width: 1257px) and (max-width:1304px) {
+.buton{
+width: 85%;
+}
+.p-button{
+    right:15px;
+}
+}
+@media (min-width: 1305px) and (max-width:1378px) {
+.buton{
+width: 85%;
+}
+.p-button{
+    right:15px;
+}
+} 
+@media (min-width: 1306px) and (max-width:1315px) {
+.buton{
+width: 84.4%;
+}
+.p-button{
+    right:15px;
+}
+} 
+@media (min-width: 1316px) and (max-width:1336px) {
+.buton{
+width: 82.9%;
+}
+.p-button{
+    right:15px;
+}
+} 
+@media (min-width: 1337px) and (max-width:1362px) {
+.buton{
+width: 81%;
+}
+.p-button{
+    right:15px;
+}
+}
+@media (min-width: 1363px) and (max-width:1379px) {
+.buton{
+width: 80.9%;
+}
+.p-button{
+    right:15px;
+}
+}
+@media (min-width: 1380px) and (max-width:1400px) {
+.buton{
+width: 80%;
 
+}
+.p-button{
+    right:15px;
+}
+}
+@media (min-width: 1400px) and (max-width:1509px) {
+.buton{
+width: 73.6%;
+
+}
+.p-button{
+    right:15px;
+}
+}
+@media (min-width: 1510px) and (max-width:1517px) {
+.buton{
+width: 73%;
+}
+.p-button{
+    right:15px;
+}
+}
+@media (min-width: 1519px) and (max-width:1539px) {
+.buton{
+width: 72.8%;
+}
+.p-button{
+    right:15px;
+}
+}
+@media (min-width: 1540px) and (max-width:1542px) {
+.buton{
+width: 71.8%;
+}
+.p-button{
+    right:15px;
+}
+}
+@media (min-width: 1543px) and (max-width:1569px) {
+.buton{
+width: 70.9%;
+}
+.p-button{
+    right:15px;
+}
+}
+@media (min-width: 1569px) and (max-width:1600px) {
+.buton{
+width: 69%;
+}
+.p-button{
+    right:15px;
+}
+}
+@media (min-width: 1601px) and (max-width:1629px) {
+.buton{
+width: 67.9%;
+
+}
+.p-button{
+    right:15px;
+}
+}
 </style>
