66963757021080244199609d8d3cb72cdd1e09f9
okon ifiok friday <wisdominterconnect@gmail.com>
2021-02-18 11:55:56 +0100

    finish page of groupattendance

--- src/views/event/attendance&checkin/GroupCategoryAttendance.vue
+++ src/views/event/attendance&checkin/GroupCategoryAttendance.vue
@@ -1,34 +1,37 @@
 <template>
     <div>
-        <div class="container" style="width:80%">
-            <div class="row mt-3 border-bottom">
-                <div class="col-md-7 col-lg-8 col-sm-12 mb-4">
+        <div class="container">
+            <div class="row  mb-4 mt-3">
+                <div class="col-md-7 col-lg-7 col-sm-12 mb-4">
                     <h1 class="aten">Attendance & Checkin</h1>
                 </div>
-                 <div class="col-md-5 col-lg-4 col-sm-12 mb-4">
+                 <div class="col-md-5 col-lg-3 col-sm-12 mb-4">
                     <Button label="Add New Attendance" class="p-button-rounded" />
                 </div>
-
             </div>
-            <div class="row mt-3 border rounded sub-con">
+            <div class="row mt-3 border m-sm-0 rounded sub-con">
                 <div class="col-md-12 col-sm-12 col-lg-12 mt-3 mb-5 border-bottom">
                     <h5 class="header5">Group Category Attendance</h5>
                 </div>
-                <div class="row m-auto" style="width:70%">
-                <div class="col-md-2 col-sm-12">
-                   <h5 class="event mt-3"> Event</h5>
-                </div>
-                <div class="col-md-10 col-sm-12">
-                    <Dropdown v-model="selectedCountry" :options="countries"  style="width:70%" optionLabel="name" :filter="true" placeholder="Workers Meeting(12th Oct, 2020)" :showClear="true">
-                    </Dropdown>
-                </div>
-                <div class="col-md-2 col-sm-12">
-                    <h5 class="event mt-4"> Group</h5>
-                </div>
-                <div class="col-md-10 mt-3 col-sm-12">
-                    <MultiSelect v-model="selectedGroup" :options="groups" style="width:70%" optionLabel="name" placeholder="Choir" display="chip"/>
-                        
-                </div>
+                <div class="row m-auto">
+                    <div class="col-md-8 offset">
+                        <div class="row">
+                            <div class="col-md-2 col-sm-12">
+                                <h5 class="event mt-3"> Event</h5>
+                            </div>
+                            <div class="col-md-10 col-sm-12">
+                                <Dropdown v-model="selectedCountry" :options="countries"  style="width:100%" optionLabel="name" :filter="true" placeholder="Workers Meeting(12th Oct, 2020)" :showClear="true">
+                                </Dropdown>
+                            </div>
+                            <div class="col-md-2 col-sm-12">
+                                <h5 class="event mt-4"> Group</h5>
+                            </div>
+                            <div class="col-md-10 mt-3 col-sm-12">
+                                <MultiSelect v-model="selectedGroup" :options="groups" style="width:100%" optionLabel="name" placeholder="Choir" display="chip"/>
+                                    
+                            </div>
+                        </div>
+                    </div>
                 
                 </div>
                 <div class="col-md-12 mt-5 mb-2 border-bottom">
@@ -37,72 +40,108 @@
                  <div class="col-md-12 mb-1">
                       <h5 class="check">Check in options</h5>
                 </div>
-                <div class="row m-auto border rounded" style="width:70%">
-                    <div class="col-md-2 col-sm-2 image mt-3">
-                        <img src="../../../assets/facebook.png" alt="marked Attendance image">
+                <div class="row w-100">
+                        <div class="col-md-10 offset-md-1 col-sm-11 offset-1  col-lg-7 offset-lg-2 border rounded">
+                            <div class="row">
+                            <div class="col-md-2 col-sm-2 image mt-3">
+                                <img src="../../../assets/facebook.png" alt="marked Attendance image">
 
-                    </div>
-                    <div class="col-md-10 col-sm-10 mt-3">
-                         <h4 class="header4"> Marked Attendance</h4>
-                        <p class="para">Lorem ipsum dolor sit amet consectetur.</p>
-                    </div>
+                            </div>
+                            <div class="col-md-10 col-sm-10  mt-3">
+                                <a href=""><h4 class="header4">Registration Link</h4></a>
+                                <p class="para">Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                            </div>
+                        </div>
                     
                 </div>
                 <div class="col-md-12 mb-3">
                 </div>
-                 <div class="row m-auto border rounded" style="width:70%">
-                    <div class="col-md-2 col-sm-2 image mt-3">
-                        <img src="../../../assets/facebook.png" alt="marked Attendance image">
+                <div class="row w-100">
+                        <div class="col-md-10 offset-md-1 col-sm-11 offset-1  col-lg-7 offset-lg-2 border rounded">
+                            <div class="row">
+                            <div class="col-md-2 col-sm-2 image mt-3">
+                                <img src="../../../assets/facebook.png" alt="marked Attendance image">
 
-                    </div>
-                    <div class="col-md-10 col-sm-10 mt-3">
-                         <h4 class="header4"> Child Checking</h4>
-                        <p class="para">Lorem ipsum dolor sit amet consectetur.</p>
-                    </div>
-                    
-                </div>
-                 <div class="col-md-12 mb-3">
-                </div>
-                 <div class="row m-auto border rounded" style="width:70%">
-                    <div class="col-md-2 col-sm-2 image mt-3">
-                        <img src="../../../assets/facebook.png" alt="marked Attendance image">
-
-                    </div>
-                    <div class="col-md-10 col-sm-10 mt-3">
-                         <h4 class="header4"> Ussd</h4>
-                        <p class="para">Lorem ipsum dolor sit amet consectetur.</p>
-                    </div>
+                            </div>
+                            <div class="col-md-10 col-sm-10  mt-3">
+                                <h4 class="header4"> Marked Attendance</h4>
+                                <p class="para">Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                            </div>
+                        </div>
                     
                 </div>
                 <div class="col-md-12 mb-3">
                 </div>
-                 <div class="row m-auto border rounded" style="width:70%">
-                    <div class="col-md-2 col-sm-2 image mt-3">
-                        <img src="../../../assets/facebook.png" alt="marked Attendance image">
+                   <div class="row w-100">
+                        <div class="col-md-10 offset-md-1 col-sm-11 offset-1  col-lg-7 offset-lg-2 border rounded">
+                            <div class="row">
+                            <div class="col-md-2 col-sm-2 image mt-3">
+                                <img src="../../../assets/facebook.png" alt="marked Attendance image">
 
-                    </div>
-                    <div class="col-md-10 col-sm-10 mt-3">
-                         <h4 class="header4"> QR</h4>
-                        <p class="para">Lorem ipsum dolor sit amet consectetur.</p>
-                    </div>
+                            </div>
+                            <div class="col-md-10 col-sm-10  mt-3">
+                                <h4 class="header4"> Child Checking</h4>
+                                <p class="para">Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                            </div>
+                        </div>
                     
                 </div>
                 <div class="col-md-12 mb-3">
                 </div>
-                 <div class="row mx-auto mb-3 border rounded" style="width:70%">
-                    <div class="col-md-2 col-sm-2 image mt-3">
-                        <img src="../../../assets/facebook.png" alt="marked Attendance image">
+                   <div class="row w-100">
+                        <div class="col-md-10 offset-md-1 col-sm-11 offset-1  col-lg-7 offset-lg-2 border rounded">
+                            <div class="row">
+                            <div class="col-md-2 col-sm-2 image mt-3">
+                                <img src="../../../assets/facebook.png" alt="marked Attendance image">
 
-                    </div>
-                    <div class="col-md-10 col-sm-10 mt-2">
-                         <h5 class="header4"> Sms</h5>
-                        <p class="para">Lorem ipsum dolor sit amet consectetur.</p>
-                    </div>
+                            </div>
+                            <div class="col-md-10 col-sm-10  mt-3">
+                                <h4 class="header4"> Ussd </h4>
+                                <p class="para">Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                            </div>
+                        </div>
                     
                 </div>
-                
-                
+                <div class="col-md-12 mb-3">
+                </div>
+                   <div class="row w-100">
+                        <div class="col-md-10 offset-md-1 col-sm-11 offset-1  col-lg-7 offset-lg-2 border rounded">
+                            <div class="row">
+                            <div class="col-md-2 col-sm-2 image mt-3">
+                                <img src="../../../assets/facebook.png" alt="marked Attendance image">
 
+                            </div>
+                            <div class="col-md-10 col-sm-10  mt-3">
+                                <h4 class="header4"> QR </h4>
+                                <p class="para">Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                            </div>
+                        </div>
+                    
+                </div>
+                <div class="col-md-12 mb-3">
+                </div>
+                   <div class="row w-100">
+                        <div class="col-md-10 offset-md-1 col-sm-11 offset-1  col-lg-7 offset-lg-2 border rounded">
+                            <div class="row">
+                            <div class="col-md-2 col-sm-2 image mt-3">
+                                <img src="../../../assets/facebook.png" alt="marked Attendance image">
+
+                            </div>
+                            <div class="col-md-10 col-sm-10  mt-3">
+                                <h4 class="header4"> Sms</h4>
+                                <p class="para">Lorem ipsum dolor sit amet consectetur.</p>
+                            </div>
+                            </div>
+                        </div>
+                    
+                </div>
+                <div class="col-md-12 mb-3">
+                </div>
             </div>
 
         </div>
@@ -152,11 +191,6 @@ color: #02172E;
 opacity: 1;
     
 }
-.image image{
-    width: 50%;
-    height: 30px;
-
-}
 .header5{
  font: var(--unnamed-font-style-normal) normal 600 24px/32px var(--unnamed-font-family-nunito-sans);
 letter-spacing: var(--unnamed-character-spacing-0);
@@ -202,7 +236,8 @@ letter-spacing: 0px;
 color: #02172E;
 opacity: 0.8;
 }
-.sub-con{
+
+.container{
     margin-bottom: 40px;
 }
 
