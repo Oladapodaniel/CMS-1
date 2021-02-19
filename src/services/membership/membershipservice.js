237949a73118e870c7290b373432c70916c254cf
Ogbara Gerrald Godstar <sengodstar@gmail.com>
2021-02-19 18:27:28 +0100

    Fix member to group

--- src/services/membership/membershipservice.js
+++ src/services/membership/membershipservice.js
@@ -41,12 +41,29 @@ const membershipService = {
     },
 
     deletePeople(data) {
-        console.log(data, "DATA");
         return new Promise((resolve, reject) => {
             axios.delete("/api/People/DeletePeoples", data)
                 .then(res => {
                     resolve(res.data);
                     // store.dispatch("setFirstTimers", res.data);
+                })
+                .catch(error => {
+                     /*eslint no-undef: "warn"*/
+                     NProgress.done();
+                    if (error.response) {
+                        reject(error.response);
+                    } else {
+                        reject(error);
+                    }
+                })
+        })
+    },
+
+    addMemberToGroup(data, groupId) {
+        return new Promise((resolve, reject) => {
+            axios.put(`/api/AssignPeopleToGroup/${groupId}`, data)
+                .then(res => {
+                    resolve(res.data);
                 })
                 .catch(error => {
                      /*eslint no-undef: "warn"*/
