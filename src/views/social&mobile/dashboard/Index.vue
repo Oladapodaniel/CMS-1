85181fcc3ecac1bf3b27c0daa9754a4d57c2dd73
okon ifiok friday <wisdominterconnect@gmail.com>
2021-03-25 18:29:15 +0100

    working with social

--- src/views/social&mobile/dashboard/Index.vue
+++ src/views/social&mobile/dashboard/Index.vue
@@ -1,6 +1,130 @@
 <template>
     <div>
+        <div class="container">
+            <div class="row">
+                <div class="mt-5 col-md-6">
+                    <h1 class=" header1">Overview</h1>
+                </div>
+                <div class="mt-5 col-md-6"> 
+                    <span class="">
+                        <i class="pi pi-search mr-3"></i>
+                     </span> Search
+                    <hr>
 
+                </div>
+
+            </div>
+            <div class="row d-flex justify-content-between mt-5">
+                <div class="col-md-2 box1">
+                    <div class="row mt-3 mb-5">
+                        <div class="col-md-6 ">
+                            <span>
+                                <i class="pi pi-facebook" style="fontSize: 2rem"></i>
+                            </span>
+                        </div>
+                         <div class="col-md-6">
+                             +1%
+
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-md-12">
+                           <h2 class="text-center">33445</h2>
+                        </div>
+                    </div>
+                    <div class="row mb-4">
+                        <div class="col-md-6">
+                            <div>Followers</div>
+                        </div>
+                         <div class="col-md-6">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2 box1">
+                    <div class="row mt-3 mb-5">
+                        <div class="col-md-6 ">
+                            <span>
+                                <i class="pi pi-twitter" style="fontSize: 2rem"></i>
+                            </span>
+                        </div>
+                         <div class="col-md-6">
+                             +1%
+
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-md-12">
+                           <h2 class="text-center">33445</h2>
+                        </div>
+                    </div>
+                    <div class="row mb-4">
+                        <div class="col-md-6">
+                            <div>Followers</div>
+                        </div>
+                         <div class="col-md-6">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2 box1">
+                    <div class="row mt-3 mb-5">
+                        <div class="col-md-6 ">
+                            <span>
+                                <i class="pi pi-instagram" style="fontSize: 2rem"></i>
+                            </span>
+                        </div>
+                         <div class="col-md-6">
+                             +1%
+
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-md-12">
+                           <h2 class="text-center">33445</h2>
+                        </div>
+                    </div>
+                    <div class="row mb-4">
+                        <div class="col-md-6">
+                            <div>Followers</div>
+                        </div>
+                         <div class="col-md-6">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2 box1">
+                    <div class="row mt-3 mb-5">
+                        <div class="col-md-6 ">
+                            <span>
+                                <i class="pi pi-whatsapp" style="fontSize: 2rem"></i>
+                                <img src="../../src/assets/social/Icon zocial-twitter.svg" alt="">
+                            </span>
+                        </div>
+                         <div class="col-md-6">
+                             +1%
+
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-md-12">
+                           <h2 class="text-center">234</h2>
+                        </div>
+                    </div>
+                    <div class="row mb-4">
+                        <div class="col-md-6">
+                            <div>Followers</div>
+                        </div>
+                         <div class="col-md-6">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+          
+
+        </div>
+     
     </div>
 </template>
 
@@ -10,6 +134,21 @@
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.header1{
+text-align: left;
+font: normal normal bold 34px/46px Nunito Sans;
+letter-spacing: 0px;
+color: #02172E;
+opacity: 1;
+
+}
+.box1{
+background: #FFFFFF 0% 0% no-repeat padding-box;
+border: 1px solid #708EB152;
+border-radius: 15px;
+opacity: 1;
+
+}
 
 </style>
