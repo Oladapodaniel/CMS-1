b4b8f47296c3f6d1fdeaab8aa203493192608e33
okon ifiok friday <wisdominterconnect@gmail.com>
2021-03-29 17:51:08 +0100

    working on social

--- src/views/social&mobile/dashboard/Index.vue
+++ src/views/social&mobile/dashboard/Index.vue
@@ -2,11 +2,11 @@
     <div>
         <div class="container">
             <div class="row">
-                <div class="mt-5 col-lg-6 col-md-6 col-sm-12">
+                <div class="mt-5 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 text-sm-center">
                     <h1 class=" header1">Overview</h1>
                 </div>
                 <div class="mt-5 col-lg-6 col-md-6 col-sm-12"> 
-                    <div class="text-sm-center">
+                    <div class="text-sm-center text-center">
                     <span class="">
                        <button class="btn btn-outline-none"> <i class="pi pi-search mr-3"></i></button>
                      </span>
@@ -20,23 +20,23 @@
             <div class="row d-lg-flex justify-content-lg-between d-md-flex justify-content-md-around mt-5">
                 <div class="col-lg-2 col-md-5 mb-md-4 mb-sm-4 mb-xl-0 mb-lg-0 mb-4 box1">
                     <div class="row mt-3 mb-5">
-                        <div class="col-md-6 text-sm-center">
+                        <div class="col-md-6 text-sm-center text-center">
                             <span>
                                  <img src="../../../assets/social/facebook.svg" alt="">
                             </span>
                         </div>
-                         <div class="col-md-6 per text-sm-center">
+                         <div class="col-md-6 per text-sm-center text-center">
                              +1%
 
                         </div>
                     </div>
                     <div class="row mt-5">
-                        <div class="col-md-12 text-lg-center text-sm-center">
+                        <div class="col-md-12 text-lg-center text-sm-center text-center">
                            <h2 class="">33445</h2>
                         </div>
                     </div>
                     <div class="row mb-4">
-                        <div class="col-md-6 text-sm-center">
+                        <div class="col-md-6 text-sm-center text-center">
                             <div>Followers</div>
                         </div>
                          <div class="col-md-6">
@@ -46,23 +46,23 @@
                 </div>
                 <div class="col-lg-2 col-md-5 box1 mb-md-4 mb-sm-4 mb-xl-0 mb-lg-0 mb-4">
                     <div class="row mt-3 mb-5">
-                        <div class="col-md-6 text-sm-center ">
+                        <div class="col-md-6 text-sm-center text-center">
                             <span>
                                  <img src="../../../assets/social/instagram.svg" alt="">
                             </span>
                         </div>
-                         <div class="col-md-6 per text-sm-center">
+                         <div class="col-md-6 per text-sm-center text-center">
                              +3%
 
                         </div>
                     </div>
                     <div class="row mt-5">
-                        <div class="col-md-12 text-lg-center text-sm-center">
+                        <div class="col-md-12 text-lg-center text-sm-center text-center">
                            <h2 class="">444</h2>
                         </div>
                     </div>
                     <div class="row mb-4">
-                        <div class="col-md-6 text-sm-center">
+                        <div class="col-md-6 text-sm-center text-center">
                             <div>Followers</div>
                         </div>
                          <div class="col-md-6">
@@ -72,23 +72,23 @@
                 </div>
                 <div class="col-lg-2 col-md-5 box1 mb-sm-4 mb-xl-0 mb-lg-0 mb-4">
                     <div class="row mt-3 mb-5">
-                        <div class="col-md-6 text-sm-center">
+                        <div class="col-md-6 text-sm-center text-center">
                             <span>
                                  <img src="../../../assets/social/twitter.svg" alt="">
                             </span>
                         </div>
-                         <div class="col-md-6 per text-sm-center">
+                         <div class="col-md-6 per text-sm-center text-center">
                              +3%
 
                         </div>
                     </div>
                     <div class="row mt-5">
-                        <div class="col-md-12 text-lg-center text-sm-center">
+                        <div class="col-md-12 text-lg-center text-sm-center text-center">
                            <h2 class="">234</h2>
                         </div>
                     </div>
                     <div class="row mb-4">
-                        <div class="col-md-6 text-sm-center">
+                        <div class="col-md-6 text-sm-center text-center">
                             <div>Followers</div>
                         </div>
                          <div class="col-md-6">
@@ -98,23 +98,23 @@
                 </div>
                 <div class="col-lg-2 col-md-5 box1 mb-sm-4 mb-xl-0 mb-lg-0 mb-4">
                     <div class="row mt-3 mb-5">
-                        <div class="col-md-6 text-sm-center">
+                        <div class="col-md-6 text-sm-center text-center">
                             <span>
                                 <img src="../../../assets/social/whatsapp.svg" alt="">
                             </span>
                         </div>
-                         <div class="col-md-6 per text-sm-center">
+                         <div class="col-md-6 per text-sm-center text-center">
                              +3%
 
                         </div>
                     </div>
                     <div class="row mt-5">
-                        <div class="col-md-12 text-lg-center text-sm-center">
+                        <div class="col-md-12 text-lg-center text-sm-center text-center">
                            <h2 class="">124</h2>
                         </div>
                     </div>
                     <div class="row mb-4">
-                        <div class="col-md-6 text-sm-center">
+                        <div class="col-md-6 text-sm-center text-center">
                             <div>Followers</div>
                         </div>
                          <div class="col-md-6">
@@ -128,7 +128,7 @@
                     <h2 class="range">Range Stats</h2>
 
                 </div>
-                <div class="col-lg-5 col-md-8 time1 d-flex justify-content-between">
+                <div class="col-lg-5 col-md-8 time1 text-lg-center text-sm-center text-center">
                     <!-- <input type="date" class="border-0 d-flex justify-content-end"> -->
                     <span> 
                         <input
@@ -138,7 +138,7 @@
                         class="border-0 calendar1"
                         required
                         />
-                    </span> -
+                    </span>-
                     <span>
                         <input
                         placeholder=""
@@ -155,10 +155,12 @@
             <div class="row mt-4">
                 <div class="col-md-8">
                     <Button label="Likes" badge="8" icon="pi pi-check" iconPos="right" class="p-button-raised p-button-rounded mt-5 mb-5" />
-                    <Chart type="bar" :data="basicData" class="mt-4" />
+                    <!-- Chart -->
+                    <!-- <h3>Multi Axis</h3> -->
+                    <Chart type="bar" :data="multiAxisData" :options="multiAxisOptions"/>
 
                 </div>
-                <div class="col-md-4 post">
+                <div class="col-md-4 post mt-sm-5 mt-5">
                     <div class="row">
                         <div class="col-lg-6 col-md-5 text-sm-center  p-3">
                             <h5 class="las">Latest post</h5>
@@ -191,8 +193,8 @@
                             </p>
                         </div>
                     </div>
-                    <div class="row d-lg-flex d-md-flex justify-content-md-between justify-content-lg-around">
-                        <div class="col-lg-3 col-md-3 col-sm-3">
+                    <div class="row d-lg-flex d-md-flex d-flex justify-content-between justify-content-md-between justify-content-lg-around">
+                        <div class="col-lg-3 col-md-3 col-sm-3 col-3">
                             <div class="row">
                                 <div class="col-md-2 text-sm-center">
                                     <span><i class="pi pi-comments text-light"></i></span>
@@ -203,7 +205,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-3 col-md-3 col-sm-3">
+                        <div class="col-lg-3 col-md-3 col-sm-3 col-3">
                             <div class="row">
                                 <div class="row">
                                 <div class="col-md-2 text-sm-center">
@@ -216,7 +218,7 @@
                             </div>
                             </div>
                         </div>
-                        <div class="col-lg-3 col-md-3 col-sm-3">
+                        <div class="col-lg-3 col-md-3 col-sm-3 col-3">
                             <div class="row">
                                 <div class="col-md-2 text-sm-center">
                                     <span><i class="pi pi-share-alt text-light"></i></span>
@@ -227,7 +229,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-3 col-md-0 col-sm-3">
+                        <div class="col-lg-3 col-md-0 col-sm-0 col-0">
                             <div class="row">
                                 <div class="col-md-2">
                                     <!-- <span><i class="pi pi-share-alt text-light"></i></span> -->
@@ -251,15 +253,15 @@
              <div class="row d-flex justify-content-between mt-5 mb-5">
                 <div class="col-md-3 like px-4 py-4 mb-sm-4 mb-lg-0 mb-md-0 mb-xl-0 mb-4">
                     <div class="row">
-                        <div class="col-lg-4 col-md-12 text-sm-center">
+                        <div class="col-lg-4 col-md-12 text-sm-center text-center">
                            <button class=" btn btn-outline-none"> <span><i class="pi pi-heart"></i></span></button>
                             
 
                         </div>
-                        <div class="col-lg-7 col-md-12 mt-2 text-sm-center">
+                        <div class="col-lg-7 col-md-12 mt-2 text-sm-center text-center">
                             <h3 class="tot">Total likes</h3>
                         </div>
-                        <div class="col-md-12 text-lg-center num mt-3 text-sm-center">
+                        <div class="col-md-12 text-lg-center num mt-3 text-sm-center text-center">
                             <h3>2445</h3>
                         </div>
 
@@ -268,15 +270,15 @@
                 </div>
                  <div class="col-md-3 like px-4 py-4 mb-sm-4 mb-lg-0 mb-md-0 mb-xl-0 mb-4">
                     <div class="row">
-                        <div class="col-lg-4 col-md-12 text-sm-center">
+                        <div class="col-lg-4 col-md-12 text-sm-center text-center">
                             <button class=" btn btn-primary"> <span><i class="pi pi-share-alt"></i></span></button>
                             
 
                         </div>
-                        <div class="col-lg-8 col-md-12 mt-2 text-sm-center">
+                        <div class="col-lg-8 col-md-12 mt-2 text-sm-center text-center">
                             <h3 class="tot">Total likes</h3>
                         </div>
-                        <div class="col-md-12 text-lg-center num mt-3 text-sm-center">
+                        <div class="col-md-12 text-lg-center num mt-3 text-sm-center text-center">
                             <h3>244</h3>
                         </div>
 
@@ -285,15 +287,15 @@
                 </div>
                  <div class="col-md-3 like px-4 py-4">
                     <div class="row">
-                        <div class="col-lg-4 col-md-12 text-sm-center">
+                        <div class="col-lg-4 col-md-12 text-sm-center text-center">
                            <button class=" btn btn-success"> <span><i class="pi pi-comments"></i></span></button>
                             
 
                         </div>
-                        <div class="col-lg-8 col-md-12  mt-2 text-sm-center">
+                        <div class="col-lg-8 col-md-12  mt-2 text-sm-center text-center">
                             <h3 class="tot">Total likes</h3>
                         </div>
-                        <div class="col-md-12 text-lg-center num mt-3 text-sm-center">
+                        <div class="col-md-12 text-lg-center num mt-3 text-sm-center text-center">
                             <h3>244</h3>
                         </div>
 
@@ -319,20 +321,69 @@ import Chart from 'primevue/chart';
         data(){
             return{
                 eventDate: new Date().toISOString().substr(0, 10),
-                basicData: {
-				labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+                	multiAxisData: {
+				labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
 				datasets: [
 					{
 						label: '100%',
-						backgroundColor: '#CCD0D5',
-						data: [65, 59, 80, 81, 56, 55, 55, 100, 50, 100, 50, 45,]
+						backgroundColor: [
+                            '#CCD0D4',
+                            '#CCD0D4',
+                            '#CCD0D4',
+                            '#CCD0D4',
+                            '#CCD0D4',
+                            '#CCD0D4',
+                            '#CCD0D4',
+                            '#CCD0D4',
+                            '#CCD0D4',
+                            '#CCD0D4',
+                            '#CCD0D4',
+                            '#CCD0D4',
+						],
+						yAxisID: 'y-axis-1',
+						data: [65, 59, 80, 81, 100, 55, 10, 30,40,50,39,70]
 					},
 					// {
-					// 	label: 'My Second dataset',
-					// 	backgroundColor: '#CCD0D5',
-					// 	data: [65, 59, 80, 81, 56, 55, 55, 100, 50, 100, 50, 45,]
+					// 	label: 'Dataset 2',
+					// 	backgroundColor: '#78909C',
+					// 	yAxisID: 'y-axis-2',
+					// 	data: [28, 48, 40, 19, 86, 27, 90, 10, 30,40,50,39,70]
 					// }
 				]
+			},
+			multiAxisOptions: {
+				responsive: true,
+				tooltips: {
+					mode: 'index',
+					intersect: true
+				},
+				scales: {
+					yAxes: [
+						{
+							type: 'linear',
+							display: false,
+							position: 'left',
+							id: 'y-axis-1',
+							ticks: {
+								min: 0,
+								max: 100
+							}
+						},
+						{
+							type: 'linear',
+							display: false,
+							position: 'right',
+							id: 'y-axis-2',
+							gridLines: {
+								drawOnChartArea: false
+							},
+							ticks: {
+								min: 0,
+								max: 100
+							}
+						}
+					]
+				}
 			},
             }
         }
