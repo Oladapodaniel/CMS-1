d4da285ca151342d9a1eced96dc51432ee776b9e
Ogbara Gerrald Godstar <sengodstar@gmail.com>
2021-04-08 16:12:23 +0100

    Change file display

--- src/views/media/file/Index.vue
+++ src/views/media/file/Index.vue
@@ -1,25 +1,26 @@
 <template>
     <div class="container-fluid">
         <div class="row my-5">
-            <div class="col-md-12 text-center my-4">
+            <!-- <div class="col-md-12 text-center my-4">
                 <span><img :src="file.imagePath" style="height:100%;width:auto;max-height:300px;max-width:100%" alt="Media File"></span>
-            </div>
+            </div> -->
 
             <div class="col-md-12 my-3">
                 <div class="row">
-                    <div class="col-md-5 mx-auto">
-                        <h5 class="primary-text">{{ fileName(file.name) }}</h5>
+                    <div class="col-md-8 mx-auto">
+                        <h5 class="primary-text">{{ fileName(file) }}</h5>
                         <span>{{ formatDate(file.dateAdded) }}</span>
                     </div>
                 </div>
             </div>
 
+
             <div class="col-md-12" v-if="Number(route.query.t) === 0">
                 <div class="row">
-                    <div class="col-md-5 mx-auto">
+                    <div class="col-md-8 mx-auto">
                         <video
                             style="width: 100%"
-                            height="240"
+                            height="340"
                             controls
                             >
                             <source :src="route.query.f" />
@@ -32,7 +33,7 @@
 
             <div class="col-md-12" v-if="route.query.t === '1'">
                 <div class="row">
-                    <div class="col-md-5 mx-auto">
+                    <div class="col-md-8 mx-auto">
                         <audio controls class="w-100 outline-none">
                             <!-- <source :src="file.filePath" type="audio/ogg"> -->
                             <source :src="route.query.f" >
@@ -41,20 +42,22 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-12" v-if="route.query.t === 2">
+            <div class="col-md-12" v-if="route.query.t === '2'">
                 <div class="row">
-                    <div class="col-md-5 mx-auto">
-                        <audio controls class="w-100 outline-none">
-                            <!-- <source :src="file.filePath" type="audio/ogg"> -->
-                            <source :src="route.query.f" >
-                            Your browser does not support the audio element.
-                        </audio>
+                    <div class="col-md-8 mx-auto">
+                        <iframe
+                            :src="`https://drive.google.com/viewerng/viewer?embedded=true&url=${route.query.f}#toolbar=0&scrollbar=0`"
+                            frameBorder="0"
+                            scrolling="auto"
+                            height="700px"
+                            width="100%"
+                        ></iframe>
                     </div>
                 </div>
             </div>
             <div class="col-md-12" v-if="route.query.t === '3'">
                 <div class="row">
-                    <div class="col-md-5 mx-auto">
+                    <div class="col-md-8 mx-auto">
                         <span><img :src="route.query.f" style="height:100%;width:auto;max-height:300px;max-width:100%" alt="Media File"></span>
                     </div>
                 </div>
@@ -76,16 +79,15 @@ import dateFormatter from '../../../services/dates/dateformatter';
             const getFileById = async () => {
                 try {
                     file.value = await media_service.getMediaById(route.query.id);
-                    console.log(file.value, "FILE");
                 } catch (error) {
                     console.log(error);
                 }
             }
 
-            const fileName = (name) => {
-                if (!name) return "Unknown"
-                if (!name.includes('https')) return name;
-                const splitted = name.split('/');
+            const fileName = (item) => {
+                if (!item.name) return item.fileBlobName;
+                if (!item.name.includes('https')) return item.name;
+                const splitted = item.name.split('/');
                 const derived = splitted[splitted.length - 1];
                 if (!derived.includes('_')) return derived;
                 return derived.split('_')[0];
