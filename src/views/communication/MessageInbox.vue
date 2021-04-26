d03fdb3d15e10bf7ee93588645f15c0c97c41f6b
Ogbara Gerrald Godstar <sengodstar@gmail.com>
2021-04-26 14:35:44 +0100

    Fix replies pagintion showing empty pages

--- src/views/communication/MessageInbox.vue
+++ src/views/communication/MessageInbox.vue
@@ -283,7 +283,7 @@ export default {
     const getRepliesByPage = async (page) => {
       try {
         const data = await communicationService.getSMSReplies(page);
-        if (data) {
+        if (data && data.length > 0) {
           replies.value = data;
           currentPage.value = page;
         }
