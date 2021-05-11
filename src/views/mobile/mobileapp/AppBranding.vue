a61c833299c191f15c0829d6043efbd43b625e24
okon ifiok friday <wisdominterconnect@gmail.com>
2021-05-11 14:28:54 +0100

    working on AppBranding

--- src/views/mobile/mobileapp/AppBranding.vue
+++ src/views/mobile/mobileapp/AppBranding.vue
@@ -33,62 +33,69 @@
                     
                 </div>
                  <div class="row mt-5 d-flex px-5 px-md-0 px-lg-0 px-xl-0 justify-content-md-between">
-                    <div class="col-md-3 col-12">
+                    <div class="col-md-3 col-12 col-sm-0">
                         
 
                     </div>
-                    <div class="col-md-5 col-12">
-                       <h5 class="primaryColour">Primary Colour</h5> 
+                    <div class="col-md-5 col-12 col-sm-8">
+                       <h5 class="primaryColour mt-4">Primary Colour</h5> 
                     </div>
-                    <div class="col-md-1 col-12" id="primaryColour">
+                    <div class="col-md-1 col-sm-4 col-12" >
+                        <label for="colorpicker"></label>
+                         <input type="color" id="colorpicker" class="no-border form-control" value="#136ACA">
                     </div>
-                    <div class="col-md-3 col-12">
+                    <div class="col-md-3 col-sm-0 col-12">
 
                     </div>
                     
                 </div>
                 <hr class="mt-2 d-none d-sm-none  d-md-block">
                  <div class="row mt-3 px-5 px-md-0 px-lg-0 px-xl-0  d-flex justify-content-md-between">
-                    <div class="col-md-3 col-12">
+                    <div class="col-md-3 col-sm-0 col-12">
                         
 
                     </div>
-                    <div class="col-md-5 col-12">
-                       <h5 class="primaryColour">Secondary Colour</h5> 
+                    <div class="col-md-5 col-12 col-sm-8">
+                       <h5 class="primaryColour mt-4">Secondary Colour</h5> 
                     </div>
-                    <div class="col-md-1 col-12" id="secondaryColour">
+                    <div class="col-md-1 col-12 col-sm-4">
+                        <label for="colorpicker"></label>
+                         <input type="color" id="colorpicker" class="no-border form-control" value="#FF5D00">
                     </div>
-                    <div class="col-md-3 col-12">
+                    <div class="col-md-3 col-12 col-sm-0">
 
                     </div>
                     
                 </div>
                 <hr class="mt-2 d-none d-sm-none  d-md-block">
                 <div class="row mt-3 px-5 px-md-0 px-lg-0 px-xl-0 d-flex justify-content-md-between">
-                    <div class="col-md-3 col-12">
+                    <div class="col-md-3 col-12 col-sm-0">
                         
 
                     </div>
-                    <div class="col-md-5 col-12">
-                       <h5 class="primaryColour">Test Colour</h5> 
+                    <div class="col-md-5 col-12 col-sm-8">
+                       <h5 class="primaryColour mt-4">Test Colour</h5> 
                     </div>
-                    <div class="col-md-1 col-12" id="TestColour">
+                    <div class="col-md-1 col-12 col-sm-4">
+                         <label for="colorpicker"></label>
+                         <input type="color" id="colorpicker" class="no-border form-control" value="#FD0604">
+                        
                     </div>
-                    <div class="col-md-3 col-12">
+                    <div class="col-md-3 col-12 col-sm-0">
 
                     </div>
                     
                 </div>
                 <hr class="mt-2 d-none d-sm-none  d-md-block">
                 <div class="row mt-5 d-flex px-5 px-md-0 px-lg-0 px-xl-0  justify-content-md-between">
-                    <div class="col-md-3 col-12">
+                    <div class="col-md-3 col-12 col-0">
                     </div>
-                    <div class="col-md-1 col-12">
+                    <div class="col-md-1 col-12 col-sm-8">
                        <h5 class="logo">Logo</h5> 
                     </div>
-                    <div class="col-md-5 col-12" id="logoBox">
+                    <div class="col-md-5 col-12 col-sm-4" id="logoBox">
                          <div class="cs-input border-0 mt-2 ml-4">
-                    <label for="imgUpload" class="choose-file">
+                    <label for="imgUpload" class="choose-file mr-sm-4">
                       Choose file
                       <input
                         type="file"
@@ -101,7 +108,36 @@
                   </div>
 
                     </div>
-                    <div class=" col-md-3 col-12">
+                    <div class=" col-md-3 col-12 col-sm0">
+
+                    </div>
+                    
+                </div>
+                <!--small sreen display-->
+                 <div class="row mt-4 d-flex justify-content-md-between px-5 px-md-0 px-xl-0 px-lg-0 d-block d-sm-block d-md-none d-lg-none d-xl-none  ">
+                    <div class="col-md-3 col-12">
+                    </div>
+                    <div class="col-md-1 col-12">
+                    </div>
+                    <div class="col-md-2 col-12 mb-2 mb-md-0 mb-lg-0 mb-xl-0">
+                        <div class="col-12 text-center">
+                        <button class="smallButton">Next</button>
+                        <div class="mt-5 d-flex justify-content-center">
+                        <div class="dash">
+
+                        </div>
+                        <div class="dash1">
+
+                        </div>
+                        <div class="dash1">
+
+                        </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-12">
+                    </div>
+                    <div class="col-md-3 col-12">
 
                     </div>
                     
@@ -110,7 +146,24 @@
             </div>
             <div class="col-md-5 col-sm-12  d-none d-sm-none  d-md-block">
                 <div class="backgroundImage">
+                <div class="button2">
+                    <button class="button1">Next</button>
+                    <div class="mt-5 d-flex justify-content-between">
+                    <div class="dash">
+
+                    </div>
+                    <div class="dash1">
+
+                    </div>
+                    <div class="dash1">
+
+                    </div>
+                    </div>
+                    </div>
+                    
+                     
                 </div>
+                
             </div>
         </div>
     </div>
@@ -151,6 +204,7 @@
     background-color: #cccccc;
     height: 100vh;
     width: 100%;
+     position: relative;
 }
 .appHeading1{
 font: normal normal 800 20px/60px Nunito Sans;
@@ -158,14 +212,24 @@ letter-spacing: 0px;
 color: #031C39;
 opacity: 1;
 }
-#primaryColour{
-width: 76px;
+/* #colorpicker{
+width: 95px;
 height: 34px;
 background: #136ACD;
 border-radius: 5px;
 opacity: 1;
+cursor: pointer;
+} */
+.no-border{
+border-radius: 20px;
+opacity: 1;
+cursor: pointer;
+border: none;
+box-shadow: none;
+outline-style: 0;
+
 }
-#secondaryColour{
+/* #secondaryColour{
 width: 76px;
 height: 34px;
 background: #FF5D00 0% 0% no-repeat padding-box;
@@ -178,7 +242,7 @@ height: 34px;
 background: #FD0604 0% 0% no-repeat padding-box;
 border-radius: 5px;
 opacity: 1;
-}
+} */
 hr{
 width: 558px;
 height: 0px;
@@ -210,7 +274,7 @@ color: #031C39;
 opacity: 1;
 
 }
-.primaryColour{
+#colorpicker{
 text-align: left;
 font: normal normal bold 18px/24px Nunito Sans;
 letter-spacing: 0px;
@@ -223,6 +287,45 @@ font: normal normal bold 24px/32px Nunito Sans;
 letter-spacing: 0px;
 color: #020E1C;
 opacity: 1;
+}
+.button1{
+background: #F1FAFF 0% 0% no-repeat padding-box;
+border: 0.20000000298023224px solid #7894A6;
+border-radius: 23px;
+opacity: 1;
+width: 180px;
+height: 46px;
+color: #136ACD;
+}
+.button2{
+    text-align: center;
+    height: 0;
+    position: absolute;
+    top: 80%;
+    right: 40%;
+   
+}
+.dash{
+width: 41px;
+height: 4px;
+background: #136ACD 0% 0% no-repeat padding-box;
+border-radius: 11px;
+opacity: 1;
+}
+.dash1{
+width: 41px;
+height: 4px;
+background: #f7f8fa 0% 0% no-repeat padding-box;
+border-radius: 11px;
+opacity: 1;
+}
+.smallButton{
+background: #F1FAFF 0% 0% no-repeat padding-box;
+border: 0.20000000298023224px solid #7894A6;
+border-radius: 23px;
+opacity: 1;
+width: 180px;
+height: 46px;
 }
 
 </style>
--- src/views/mobile/mobileapp/SocialMedia.vue
+++ src/views/mobile/mobileapp/SocialMedia.vue
@@ -33,12 +33,12 @@
                     
                 </div>
                 <div class="row mt-5 d-flex justify-content-md-between px-5 px-md-0 px-xl-0 px-lg-0">
-                    <div class="col-md-3 col-12">
+                    <div class="col-md-3 col-12 col-sm-0">
                     </div>
-                    <div class="col-md-1 col-12 mb-2 mb-md-0 mb-lg-0 mb-xl-0">
+                    <div class="col-md-1 col-12 col-sm-1 mb-2 mb-md-0 mb-lg-0 mb-xl-0">
                        <img src="../../../assets/social/facebook.svg" alt="">
                     </div>
-                    <div class="col-md-5 col-12" id="logoBox">
+                    <div class="col-md-5 col-12 col-sm-11" id="logoBox">
                         <input type="text" class="w-100 py-1 border-0 outline-none px-2">
 
                     </div>
@@ -48,12 +48,12 @@
                     
                 </div>
                 <div class="row mt-4 d-flex justify-content-md-between px-5 px-md-0 px-xl-0 px-lg-0">
-                    <div class="col-md-3 col-12">
+                    <div class="col-md-3 col-12 col-sm-0">
                     </div>
-                    <div class="col-md-1 col-12 mb-2 mb-md-0 mb-lg-0 mb-xl-0">
+                    <div class="col-md-1 col-12 col-sm-1 mb-2 mb-md-0 mb-lg-0 mb-xl-0">
                        <img src="../../../assets/social/twitter.svg" alt="">
                     </div>
-                    <div class="col-md-5 col-12" id="logoBox">
+                    <div class="col-md-5 col-12 col-sm-11" id="logoBox">
                          <input type="text" class="w-100 py-1 border-0 outline-none px-2">
 
                     </div>
@@ -63,12 +63,12 @@
                     
                 </div>
                 <div class="row mt-4 d-flex justify-content-md-between px-5 px-md-0 px-xl-0 px-lg-0">
-                    <div class="col-md-3 col-12">
+                    <div class="col-md-3 col-12 col-sm-0">
                     </div>
-                    <div class="col-md-1 col-12 mb-2 mb-md-0 mb-lg-0 mb-xl-0">
+                    <div class="col-md-1 col-12 col-sm-1 mb-2 mb-md-0 mb-lg-0 mb-xl-0">
                        <img src="../../../assets/social/instagram.svg" alt="">
                     </div>
-                    <div class="col-md-5 col-12" id="logoBox">
+                    <div class="col-md-5 col-12 col-sm-11" id="logoBox">
                          <input type="text" class="w-100 py-1 border-0 outline-none px-2">
                     </div>
                     <div class="col-md-3 col-12">
@@ -77,32 +77,61 @@
                     
                 </div>
                  <div class="row mt-4 d-flex justify-content-md-between px-5 px-md-0 px-xl-0 px-lg-0">
-                    <div class="col-md-3 col-12">
+                    <div class="col-md-3 col-12 col-sm-0">
                     </div>
-                    <div class="col-md-1 col-2 mb-2 mb-md-0 mb-lg-0 mb-xl-0">
-                       <img src="../../../assets/social/instagram.svg" alt="">
+                    <div class="col-md-1 col-2 col-sm-1 mb-2 mb-md-0 mb-lg-0 mb-xl-0">
+                       <img src="../../../assets/mobileonboarding/youtube.svg" alt="">
                     </div>
-                    <div class="col-md-2 col-10 mb-0 mb-md-0 mb-lg-0 mb-xl-0 mt-2 mt-md-0 mt-lg-0 mt-xl-0">
+                    <div class="col-md-2 col-sm-2 col-10 mb-0 mb-md-0 mb-lg-0 mb-xl-0 mt-2 mt-md-0 mt-lg-0 mt-xl-0">
                         <h6 class="">YouTube Url</h6>
                     </div>
-                    <div class="col-md-3 col-12" id="logoBox">
+                    <div class="col-md-3 col-sm-9 col-12" id="logoBox">
                          <input type="text" class="w-100 py-1 border-0 outline-none px-2">
                     </div>
-                    <div class="col-md-3 col-12">
+                    <div class="col-md-3 col-sm-0 col-12">
 
                     </div>
                     
                 </div>
                  <div class="row mt-4 d-flex justify-content-md-between px-5 px-md-0 px-xl-0 px-lg-0">
+                    <div class="col-md-3 col-sm-0 col-12">
+                    </div>
+                    <div class="col-md-1 col-sm-1 col-12">
+                    </div>
+                    <div class="col-md-2 col-sm-2 col-12 mb-2 mb-md-0 mb-lg-0 mb-xl-0">
+                        <h6 class="p-0">Channel ID</h6>
+                    </div>
+                    <div class="col-md-3 col-sm-9 col-12" id="logoBox">
+                         <input type="text" class="w-100 py-1 border-0 outline-none px-2">
+                    </div>
+                    <div class="col-md-3 col-12 col-sm-0">
+
+                    </div>
+                    
+                </div>
+                <!--small sreen display-->
+                 <div class="row mt-4 d-flex justify-content-md-between px-5 px-md-0 px-xl-0 px-lg-0 d-block d-sm-block d-md-none d-lg-none d-xl-none  ">
                     <div class="col-md-3 col-12">
                     </div>
                     <div class="col-md-1 col-12">
                     </div>
                     <div class="col-md-2 col-12 mb-2 mb-md-0 mb-lg-0 mb-xl-0">
-                        <h6 class="p-0">Channel ID</h6>
+                        <div class="col-12 text-center">
+                        <button class="smallButton">Next</button>
+                        <div class="mt-5 d-flex justify-content-center">
+                        <div class="dash1">
+
+                        </div>
+                        <div class="dash1">
+
+                        </div>
+                        <div class="dash">
+
+                        </div>
+                        </div>
+                        </div>
                     </div>
-                    <div class="col-md-3 col-12" id="logoBox">
-                         <input type="text" class="w-100 py-1 border-0 outline-none px-2">
+                    <div class="col-md-3 col-12">
                     </div>
                     <div class="col-md-3 col-12">
 
@@ -113,7 +142,23 @@
             </div>
             <div class="col-md-5">
                 <div class="backgroundImage d-none d-sm-none  d-md-block">
+                    <div class="button2">
+                    <button class="button1">Next</button>
+                    <div class="mt-5 d-flex justify-content-between">
+                    <div class="dash1">
+
+                    </div>
+                    <div class="dash1">
+
+                    </div>
+                    <div class="dash">
+
+                    </div>
+                    </div>
+                    </div>
+                   
                 </div>
+                
             </div>
         </div>
     </div>
@@ -227,5 +272,45 @@ letter-spacing: 0px;
 color: #020E1C;
 opacity: 1;
 }
+.button1{
+background: #F1FAFF 0% 0% no-repeat padding-box;
+border: 0.20000000298023224px solid #7894A6;
+border-radius: 23px;
+opacity: 1;
+width: 180px;
+height: 46px;
+color: #136ACD;
+}
+.button2{
+    text-align: center;
+    height: 0;
+    position: absolute;
+    top: 80%;
+    right: 40%;
+   
+}
+.dash{
+width: 41px;
+height: 4px;
+background: #136ACD 0% 0% no-repeat padding-box;
+border-radius: 11px;
+opacity: 1;
+}
+.dash1{
+width: 41px;
+height: 4px;
+background: #f7f8fa 0% 0% no-repeat padding-box;
+border-radius: 11px;
+opacity: 1;
+}
+.smallButton{
+background: #F1FAFF 0% 0% no-repeat padding-box;
+border: 0.20000000298023224px solid #7894A6;
+border-radius: 23px;
+opacity: 1;
+width: 180px;
+height: 46px;
+}
+
 
 </style>
