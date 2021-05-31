4f8235472f3e9eb6f388edb98ffc46e03f8d79b9
Chijioke <anichijioke81@gmail.com>
2021-05-31 18:10:05 +0100

    image responsive

--- src/views/mobile/mobileapp/ChurchSetUp.vue
+++ src/views/mobile/mobileapp/ChurchSetUp.vue
@@ -44,10 +44,11 @@
               <!-- <div class="col-sm-12 text-right align-self-center mt-2"></div> -->
               <div class="col-12 mt-4">
                 <div class="row">
-                  <div class="col-md-4 col-12 col-sm-5 fill">
+                  <div class="col-md-4 col-12 col-sm-5 person-image">
                     <img
+                      style="width: 110px; height: 110px; border-radius: 50%;"
                       :src="item.url"
-                      class="w-100 "
+                      class="w-75 "
                     />
                   </div>
                   <div class="col-10 col-sm-5 align-self-center">
@@ -838,5 +839,9 @@ export default {
   border: 1px solid rgb(62, 68, 160);;
   transition: all 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
   cursor: pointer;
+}
+.person-image {
+  width: 50%;
+  margin: auto;
 }
 </style>
