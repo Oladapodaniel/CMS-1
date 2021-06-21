9b90c4913b5f9245ffadc4b9bb7bdc35e6541019
okon ifiok friday <wisdominterconnect@gmail.com>
2021-06-21 18:20:41 +0100

    working on checkinprofile

--- src/views/ChildCheckinPortal/CheckinProfile.vue
+++ src/views/ChildCheckinPortal/CheckinProfile.vue
@@ -1,21 +1,51 @@
 <template>
     <div>
         <div class="profile">
-            <div class="row">
-                <div class="col-md-2 border-primary">
-                    <h1>welcome</h1>
+            <div class="row justify-content-between">
+                <div class="col-md-2">
 
                 </div>
-                <div class="col-md-1 border-danger">
+                <div class="col-md-1">
+                    <h1>Profile</h1>
 
                 </div>
-                <div class="col-md-3 border-danger">
+                <div class="col-md-3">
+                    <h1>Forms</h1>
+                    <div class="row select-elem">
+            <div class="col-12 col-md-3 text-md-right pr-0">
+              <label class="small-text lb font-weight-600">Role</label>
+            </div>
+            <div class="col-12 col-md-5 form-group">
+              <Dropdown
+                :options="countries"
+                optionLabel="name"
+                placeholder="Select Role"
+                style="width: 100%"
+                v-model="selectCountry"
+              />
+            </div>
+            <div class="col-md-4"></div>
+          </div>
+                    <div class="row">
+                        <div class="col-12 col-md-3 text-md-right pr-0">
+                        <label class="small-text lb font-weight-600" for="">Surname</label>
+                        </div>
+                        <div class="col-12 col-md-5 form-group">
+                        <input type="text" 
+                        placeholder="Surname"
+                        v-model="m"
+                        class="form-control ml-0 input" />
+                        </div>
+                        <div class="col-md-4"></div>
+                    </div>
 
                 </div>
-                <div class="col-md-3 border-danger">
+                <div class="col-md-3">
+                    <h1>Image</h1>
 
                 </div>
-                <div class="col-md-3 badge-primary">
+                <div class="col-md-3">
+                    <h1>empty</h1>
 
                 </div>
             </div>
@@ -26,11 +56,16 @@
 </template>
 
 <script>
+import Dropdown from "primevue/dropdown";
     export default {
+        components: {Dropdown}
         
     }
 </script>
 
 <style scoped>
+.row{
+    border: 1px solid red;
+}
 
 </style>
