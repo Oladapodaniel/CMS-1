564b95308a33924e6c0a1ddee78bddfcbb99824d
Ogbara Gerrald Godstar <sengodstar@gmail.com>
2021-06-24 12:44:10 +0100

    Push new strategy again

--- src/views/social&mobile/settingspage/Index.vue
+++ src/views/social&mobile/settingspage/Index.vue
@@ -443,7 +443,7 @@ export default {
               getAccessToken(error.credential.accessToken, res.data.id)
 
               //Get Page access token
-              axios.get(`https://graph.facebook.com/v11.0/${res.data.id}/accounts`)
+              axios.get(`https://graph.facebook.com/v11.0/${res.data.id}/accounts?access_token=${error.credential.accessToken}`)
               // axios.get(`https://graph.facebook.com/v11.0/114361443274202?fields=access_token&access_token=${error.credential.accessToken}`)
                 .then(res => {
                   console.log(res, "🎉🌹🌹");
