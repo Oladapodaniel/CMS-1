91b2153eb030fecc9bbb2b8be12e925059d6387d
Otuedor Gideon <samsyotuedor40@gmail.com>
2021-07-30 11:05:05 +0100

    add some logic

--- src/main.js
+++ src/main.js
@@ -57,7 +57,6 @@ axios.interceptors.request.use((config) => {
   
   // before a response is returned stop nprogress
   axios.interceptors.response.use(response => {
-  console.log(response, 'giddyssman')
     NProgress.done()
     return response
   })
