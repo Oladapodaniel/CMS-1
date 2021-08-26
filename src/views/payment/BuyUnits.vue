9077fd5cc995ff8c3bff5602cc85536a01cec46f
Oladapo Daniel <oladapodaniel10@gmail.com>
2021-08-26 11:34:33 +0100

    changed test key to live for paystack

--- src/views/payment/BuyUnits.vue
+++ src/views/payment/BuyUnits.vue
@@ -352,8 +352,8 @@ export default {
       console.log(userEmail.value, "UE");
       /*eslint no-undef: "warn"*/
       let handler = PaystackPop.setup({
-        // key: process.env.VUE_APP_PAYSTACK_PUBLIC_KEY_LIVE,
-        key: process.env.VUE_APP_PAYSTACK_API_KEY,
+        key: process.env.VUE_APP_PAYSTACK_PUBLIC_KEY_LIVE,
+        // key: process.env.VUE_APP_PAYSTACK_API_KEY,
         email: userEmail.value,
         amount: amount.value * 100,
         firstname: churchName.value,
