43282d648a8adcb132c71fd6f1a1721457fd9ce8
Otuedor Gideon <samsyotuedor40@gmail.com>
2021-09-03 14:46:48 +0100

    Create new file

--- src/router/index.js
+++ src/router/index.js
@@ -279,6 +279,24 @@ const routes = [
                     import ( /* webpackChunkName: "addfirsttimer" */ '../views/Reports/Performancereport/GroupCategoryReport.vue')
             },
             {
+                path: 'firsttimerperformancereport',
+                name:"FirstTimerPerformanceReport",
+                meta: {
+                    title: 'Churchplus - Reports',
+                },
+                component: () =>
+                    import ( /* webpackChunkName: "addfirsttimer" */ '../views/Reports/Performancereport/FirstTimerPerformanceReport.vue')
+            },
+            {
+                path: 'newconvertperformancereport',
+                name:"NewConvertPerformanceReport",
+                meta: {
+                    title: 'Churchplus - Reports',
+                },
+                component: () =>
+                    import ( /* webpackChunkName: "addfirsttimer" */ '../views/Reports/Performancereport/NewConvertPerformanceReport.vue')
+            },
+            {
                 path: 'family',
                 name: 'Family',
                 component: () =>
