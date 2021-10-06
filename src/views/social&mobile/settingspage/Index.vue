d3065696b56d9b58816fa0dcd1801a8ffb7d9bf4
okon ifiok friday <wisdominterconnect@gmail.com>
2021-10-06 15:05:04 +0100

    working on facebook

--- src/views/social&mobile/settingspage/Index.vue
+++ src/views/social&mobile/settingspage/Index.vue
@@ -464,46 +464,32 @@ export default {
       return display1.value = true
 
     }
-    //Login facebook New
-     const statusChangeCallback =(response) => {  // Called with the results from FB.getLoginStatus().
-    console.log('statusChangeCallback');
-    console.log(response);                   // The current login status of the person.
-    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
-      testAPI();  
-    } else {                                 // Not logged into your webpage or we are unable to tell.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this webpage.';
-    }
-  }
+  //   //Login facebook New
+  //    const statusChangeCallback =(response) => {  // Called with the results from FB.getLoginStatus().
+  //   console.log('statusChangeCallback');
+  //   console.log(response);                   // The current login status of the person.
+  //   if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+  //     testAPI();  
+  //   } else {                                 // Not logged into your webpage or we are unable to tell.
+  //     document.getElementById('status').innerHTML = 'Please log ' +
+  //       'into this webpage.';
+  //   }
+  // }
   
     const facebookLogin = () => {               // Called when a person is finished with the Login Button.
       FB.getLoginStatus(function(response) {   // See the onlogin handler
-        statusChangeCallback(response);
-      });
-
-      window.fbAsyncInit = () => {
-        FB.init({
-          appId      : '479314263454002',
-          cookie     : true,                     // Enable cookies to allow the server to access the session.
-          xfbml      : true,                     // Parse social plugins on this webpage.
-          version    : 'v12.0'           // Use this Graph API version for this call.
-        });
-
-
-        FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
-          statusChangeCallback(response);        // Returns the login status.
-        });
-      };
-    }
-
-    const testAPI = () => {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
-      console.log('Welcome!  Fetching your information.... ');
-      FB.api('/me', function(response) {
-        console.log('Successful login for: ' + response.name);
-        document.getElementById('status').innerHTML =
-          'Thanks for logging in, ' + response.name + '!';
+        console.log(response);
       });
     }
+
+    // const testAPI = () => {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+    //   console.log('Welcome!  Fetching your information.... ');
+    //   FB.api('/me', function(response) {
+    //     console.log('Successful login for: ' + response.name);
+    //     document.getElementById('status').innerHTML =
+    //       'Thanks for logging in, ' + response.name + '!';
+    //   });
+    // }
 
     //Login facebook New
 
@@ -725,9 +711,7 @@ export default {
       facebookLogin,
       // pageAccessToken,
       getPageAccessToken,
-      getSocialMediaContact,
-      statusChangeCallback,
-      testAPI
+      getSocialMediaContact
     };
   },
 };
