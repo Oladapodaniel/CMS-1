ab6747c44f90b721b00005dd229d15f1d462ceb2
Otuedor Gideon <samsyotuedor40@gmail.com>
2021-10-26 06:22:14 +0100

    create new file

---
+++ src/views/people/RegisterPersonID.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container-fluid container-wide">
+        <RegisterpersonId/>
+    </div>
+</template>
+
+<script>
+
+import RegisterpersonId from '../../views/people/RegisterPersonID.vue'
+export default {
+    components :{
+        RegisterpersonId
+    },
+    setup() {
+        
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
