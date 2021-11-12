1d045ebf1b81c34545310fa25c295452525a7718
Oladapo Daniel <oladapodaniel10@gmail.com>
2021-11-12 16:20:56 +0100

    changed base url

--- src/gateway/backendapi.js
+++ src/gateway/backendapi.js
@@ -2,9 +2,10 @@ import axios from 'axios'
 
 export default axios.create({
     // baseURL: 'https://ecofaith.azurewebsites.net/', //local
-     baseURL: 'https://churchplusv3coreapi.azurewebsites.net/',
+    //  baseURL: 'https://churchplusv3coreapi.azurewebsites.net/',
     // baseURL: 'https://unionfaith.azurewebsites.net/',
     // baseURL: 'http://192.168.0.118/',
+     baseURL: 'https://churchplusapi3v.azurewebsites.net/',
     timeout: 100000,
     headers: {
         'Content-Type': 'application/json',
