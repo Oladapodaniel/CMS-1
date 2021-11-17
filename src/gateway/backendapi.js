08ac4a21bbd2d4b0ad2ccfb8c09f163c121a512a
Oladapo Daniel <oladapodaniel10@gmail.com>
2021-11-17 16:16:23 +0100

    included https

--- src/gateway/backendapi.js
+++ src/gateway/backendapi.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default axios.create({
     // baseURL: 'https://ecofaith.azurewebsites.net/', //local
-     baseURL: 'http://churchplusapi3v.azurewebsites.net/',
+     baseURL: 'https://churchplusapi3v.azurewebsites.net/',
      
     // baseURL: 'https://unionfaith.azurewebsites.net/',
     timeout: 100000,
